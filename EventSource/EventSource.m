1da956ce5991e69c1204eb001159b32f5fbe8e77
Danial Zahid <danialzahid94@live.com>
2017-03-16 22:16:04 +0500

    Optimize backoff

--- EventSource/EventSource.m
+++ EventSource/EventSource.m
@@ -73,7 +73,7 @@ static NSString *const ESEventRetryKey = @"retry";
         _mobileKey = mobileKey;
         messageQueue = dispatch_queue_create("co.cwbrn.eventsource-queue", DISPATCH_QUEUE_SERIAL);
         connectionQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0);
-
+        
         dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(_retryInterval * NSEC_PER_SEC));
         dispatch_after(popTime, connectionQueue, ^(void){
             [self _open];
@@ -117,8 +117,6 @@ static NSString *const ESEventRetryKey = @"retry";
     [self.eventSourceTask cancel];
 }
 
-// -----------------------------------------------------------------------------------------------------------------------------------------
-
 - (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask
 didReceiveResponse:(NSURLResponse *)response completionHandler:(void (^)(NSURLSessionResponseDisposition disposition))completionHandler
 {
@@ -127,11 +125,12 @@ didReceiveResponse:(NSURLResponse *)response completionHandler:(void (^)(NSURLSe
         // Opened
         Event *e = [Event new];
         e.readyState = kEventStateOpen;
-
+        
+        _retryAttempt = 0;
         [self _dispatchEvent:e type:ReadyStateEvent];
         [self _dispatchEvent:e type:OpenEvent];
     }
-
+    
     if (completionHandler) {
         completionHandler(NSURLSessionResponseAllow);
     }
@@ -141,34 +140,34 @@ didReceiveResponse:(NSURLResponse *)response completionHandler:(void (^)(NSURLSe
 {
     NSString *eventString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
     NSArray *lines = [eventString componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]];
-
+    
     Event *event = [Event new];
     event.readyState = kEventStateOpen;
-
+    
     for (NSString *line in lines) {
         if ([line hasPrefix:ESKeyValueDelimiter]) {
             continue;
         }
-
+        
         if (!line || line.length == 0) {
-                dispatch_async(messageQueue, ^{
-                    [self _dispatchEvent:event];
-                });
-
-                event = [Event new];
-                event.readyState = kEventStateOpen;
+            dispatch_async(messageQueue, ^{
+                [self _dispatchEvent:event];
+            });
+            
+            event = [Event new];
+            event.readyState = kEventStateOpen;
             continue;
         }
-
+        
         @autoreleasepool {
             NSScanner *scanner = [NSScanner scannerWithString:line];
             scanner.charactersToBeSkipped = [NSCharacterSet whitespaceCharacterSet];
-
+            
             NSString *key, *value;
             [scanner scanUpToString:ESKeyValueDelimiter intoString:&key];
             [scanner scanString:ESKeyValueDelimiter intoString:nil];
             [scanner scanUpToCharactersFromSet:[NSCharacterSet newlineCharacterSet] intoString:&value];
-
+            
             if (key && value) {
                 if ([key isEqualToString:ESEventEventKey]) {
                     event.event = value;
@@ -192,20 +191,20 @@ didReceiveResponse:(NSURLResponse *)response completionHandler:(void (^)(NSURLSe
 - (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(nullable NSError *)error
 {
     self.eventSourceTask = nil;
-
+    
     if (wasClosed) {
         return;
     }
-
+    
     Event *e = [Event new];
     e.readyState = kEventStateClosed;
     e.error = error ?: [NSError errorWithDomain:@""
-                                  code:e.readyState
-                              userInfo:@{ NSLocalizedDescriptionKey: @"Connection with the event source was closed." }];
-
+                                           code:e.readyState
+                                       userInfo:@{ NSLocalizedDescriptionKey: @"Connection with the event source was closed." }];
+    
     [self _dispatchEvent:e type:ReadyStateEvent];
     [self _dispatchEvent:e type:ErrorEvent];
-
+    
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)([self increaseIntervalWithBackoff] * NSEC_PER_SEC));
     dispatch_after(popTime, connectionQueue, ^(void){
         [self _open];
@@ -220,19 +219,19 @@ didReceiveResponse:(NSURLResponse *)response completionHandler:(void (^)(NSURLSe
     if (self.lastEventID) {
         [request setValue:self.lastEventID forHTTPHeaderField:@"Last-Event-ID"];
     }
-
+    
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]
                                                           delegate:self
                                                      delegateQueue:[NSOperationQueue currentQueue]];
-
+    
     self.eventSourceTask = [session dataTaskWithRequest:request];
     [self.eventSourceTask resume];
-
+    
     Event *e = [Event new];
     e.readyState = kEventStateConnecting;
-
+    
     [self _dispatchEvent:e type:ReadyStateEvent];
-
+    
     if (![NSThread isMainThread]) {
         CFRunLoopRun();
     }
@@ -251,7 +250,7 @@ didReceiveResponse:(NSURLResponse *)response completionHandler:(void (^)(NSURLSe
 - (void)_dispatchEvent:(Event *)event
 {
     [self _dispatchEvent:event type:MessageEvent];
-
+    
     if (event.event != nil) {
         [self _dispatchEvent:event type:event.event];
     }
@@ -259,13 +258,12 @@ didReceiveResponse:(NSURLResponse *)response completionHandler:(void (^)(NSURLSe
 
 - (CGFloat)increaseIntervalWithBackoff {
     _retryAttempt++;
-    CGFloat retryInterval = MIN(ES_MAX_RECONNECT_TIME, _retryInterval * pow(2, _retryAttempt));
+    CGFloat retryInterval = MIN(ES_MAX_RECONNECT_TIME, _retryInterval * pow(_retryAttempt, 2));
     return retryInterval;
 }
 
 @end
 
-// ---------------------------------------------------------------------------------------------------------------------
 
 @implementation Event
 
@@ -273,13 +271,13 @@ didReceiveResponse:(NSURLResponse *)response completionHandler:(void (^)(NSURLSe
 {
     NSString *state = nil;
     switch (self.readyState) {
-        case kEventStateConnecting:
+            case kEventStateConnecting:
             state = @"CONNECTING";
             break;
-        case kEventStateOpen:
+            case kEventStateOpen:
             state = @"OPEN";
             break;
-        case kEventStateClosed:
+            case kEventStateClosed:
             state = @"CLOSED";
             break;
     }
